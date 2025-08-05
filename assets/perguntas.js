a92e6fe476550f1da297b830461d67eebecd0838
Yasmim35634 <hunhoff.silva@escola.pr.gov.br>
2025-08-05 16:27:53 -0300

    05//08

---
+++ assets/perguntas.js
@@ -0,0 +1,18 @@
+criaCartao(
+    'Serviços de entregas da Kiki',
+'Logo no inicio do filme a Kiki resolve se mudar para onde?',
+'Para uma cidade com visão para o mar'
+)
+
+criaCartao(
+    'Ponio',
+'no inicio do filme a Ponio é um?',
+'Peixe, igual um peixe dourado'
+)
+
+criaCartao(
+    'A viagem de Chihiro',
+'No inicio do filme a Chihiro começa a fugir após ver?',
+'Que seus pais haviam se transformado em porcos'
+)
+
