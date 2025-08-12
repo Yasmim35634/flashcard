c6b908aa84ce40e76a2a658d8395007ed4dcbf97
Yasmim35634 <hunhoff.silva@escola.pr.gov.br>
2025-08-12 16:27:37 -0300

    12/08

--- assets/perguntas.js
+++ assets/perguntas.js
@@ -16,3 +16,21 @@ criaCartao(
 'Que seus pais haviam se transformado em porcos'
 )
 
+
+criaCartao(
+    'Meu Amigo Totoro',
+'A irma mais velha ou mais nova encontra o totoro primeiro?',
+'A mais nova'
+)
+
+criaCartao(
+    'Mundo Dos Pequeninos',
+'A primeira interação entre o menino e a pequenina acontece quando?',
+'A pequenina vai buscar açucar na casa dos gigantes'
+)
+
+criaCartao(
+    'O Castelo Animado',
+'Qual animal o Howl vira?',
+'Um Passáro'
+)
